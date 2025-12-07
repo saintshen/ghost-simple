9b17da02854c7beb840f7e51e4bda6139ad01c4c
Ming Shen <ming@hs-labs.com>
2025-12-06 21:48:26 -0500

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+// JavaScript files are compiled and minified during the build process to the assets/built folder. See available scripts in the package.json file.
+
+// Import CSS
+import "../css/index.css";
+
+// Import JS
+import menuOpen from "./menuOpen";
+import infiniteScroll from "./infiniteScroll";
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Call the menu and infinite scroll functions
+// menuOpen();
+infiniteScroll();
+
+const swiper = new Swiper('.swiper',{
+    modules: [Navigation, Pagination],
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
